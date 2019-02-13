8d4755cdd828e6727e261dedc302c43822c4792d
John Sexton <john.a.sexton@gmail.com>
2019-02-12 20:38:57 -0500

    final commit from intro class Pie and Python

---
+++ Intro_to_Python/examples/importing_modules/temp.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+# this prints "hello class" to the screen
+print('hello class')
+
+# VARIABLES
+
+# atomic types of variables are string, integers and booleans
+
+# String
+name = "John"
+
+# Int
+age = "51"
+
+# boolean "True" and "False" are case sensitive and important for conditional logic
+bool_true = True
+bool_false = False 
+
+# concatenate your variables into a larger string using String and Int types
+print("My name is", name, "and I am", age, "years old")
+print(bool_true, "or", bool_false)
+
+# COLLECTIONS
+# list positions start with "position-zero" so '1' = position-zero
+# list is defined by the [] square brackets, not the name or the contents which can be anything
+a_list = ['1', '2', '3'] 
+
+# use your collection and print position-zero
+print(a_list[0])
+
+# now change the 1 to a 4
+# lists can be modified
+a_list[1] = '4'
+
+# tuple is another kind of collection
+# a tuple is defined by the () parenthesees not the name, or contents which can be anything
+a_tuple = (1, 2, 3)
+print(a_tuple[1])
+
+# modifying a tuple will throw an error since tuples are immutable
+# a_tuple[1] = 5 # commented out since it is immutable it errors
+
+# DICTIONARY is another type of collection
+# these are 'key: value' pairs so you query the key and get the value
+# dictionary is defined by the {} curly braces not the variable name or the contents which can be anything
+a_dict = {
+        "one": 1,
+        "two": '2',
+        "three": 3
+        }
+print(a_dict["three"])
+
+# LOOPS 
+# perform an action on every item in a collection 
+# 2 main types are: while & for...in
+numbers = [1, 2, 3, 4, 5]
+i = 0 # set your index value to position-zero
+sum = 0 # set another variable called "sum" to 0
+while i < len(numbers): # define while loop condition and terminate with a ":" colon     
+    # it will result in a boolean True or False
+    sum += numbers[i] # equivalent to sum = sum + <value from collection>
+    i += 1
+    # once the conditional while loop evaluates to a boolean True, it will loop
+    # once the condition evaluates as a boolean False, it will EXIT the while loop
+    # this will trigger the NEXT line of code that is OUTSIDE the while loop 
+    # inside or outside of the loop is all controlled by indentation or not
+    # indented is inside, and flush left is outside the structure
+    print(sum)
+
+
+# for...in loop is a different type of loop
+for number in numbers:  # automatically starts at position-zero and puts it into the variable "number" 
+    sum += number
+    print(sum)
+
+# LOGICAL BRANCHING
+
+# IF Statements
+age = 18
+if age < 21:
+    print('no booze for you')
+if age > 21:
+    print('have a drink on me')
+    
+age = 24
+if age < 21:
+    print('no booze for you')
+if age > 21:
+    print('have a drink on me')    
+
+# IF THEN Statement
+# you get a boolean True evaluation, it exits the loop and doesn't execute anything else
+age = 21    
+if age < 21:
+    print('no booze for you')
+elif age > 21: # it is "elif" in python NOT "elseif" like other languages
+    print('have a drink on me')
+else: # else is the "default case" when everything above fails to produce a boolean true
+    print('Happy 21st!')
+
+# FUNCTIONS
+# begin with "def" for Define
+# this is a function defined as "print_age"
+def print_age(age):
+    print("I am", age) 
+    
+# now use the print_age function
+print_age(32)
+print_age(332)
+print_age(325)
+
+def get_age(age):
+    return "you are " + str(age) # the "+" operator on Strings will execute a concatenation 
+
+print(get_age(32))
+
+import matplotlib.pyplot as plt
+# without the alias "plt" you would have to type out "matplotlib.pyplot"
+# in order to plot you need x-axis values, and y-axis values
+x_values = [1, 2, 3, 4, 5] # numbers
+y_values = [1, 4, 9, 16, 25] # squares
+plt.scatter(x_values, y_values)
+plt.show
+
+import matplotlib.pyplot as plt2
+x_values = list(range(1, 101)) # numbers startng with 1 will end with 100
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values)
+plt2.show
+
+import matplotlib.pyplot as plt3
+x_values = list(range(1, 101)) # numbers startng with 1 will end with 100
+y_values = [x**2 for x in x_values]
+plt.title("Squared Values", fontsize=24)
+plt.xlabel("Number")
+plt.ylabel("Squared") 
+plt.scatter(x_values, y_values)
+plt3.show
+
+import matplotlib.pyplot as plt3
+import json
+x_values = []
+y_values = []
+with open('georgia_unemployment.json') as file:
+    unemp_file = json.load(file) 
+    for entry in unemp_file["data"]: 
+        x_values.append(entry["month"])
+        y_values.append(entry["rate"])
+plt.title("Georgia Unemployment", fontsize=24)
+plt.xlabel("Month")
+plt.ylabel("Rate") 
+plt.plot(x_values, y_values)
+plt3.show
--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# PieAndPython
-Python tutorial examples
+Pie an Python Class
+
+DOCS: 
+https://generalassemb.ly/education/pie-python/atlanta/67110?utm_campaign=cwe_confirmation&utm_content=instance_confirmation_email&utm_medium=ga_email&utm_name=instance_confirmation_email&utm_source=core_cwe
+
+STEPS:
+install Anaconda for Python 3.6 or higher
+install Microsoft VsCode when prompted
+launch Anaconda Navigator
+add anaconda3 to your path in ~/.bashrc
+export CONDA_PATH=/Users/johnsexton/anaconda3
+export PATH=$M2_HOME/bin$ISTIO_HOME/bin:$CONDA_PATH/bin:$PATH
+
+CLASS:
+instructor: Mike Sanders - Pivotal Labs
+full stack developer since 2006
+all slides are in the PDF in the download, which I stashed into a git repo:
+https://github.com/johnasexton/PieAndPython
+
+IDE
+Anaconda - Python 3.7 for this class
+Atom https://atom.io 
+PyCharm https://www.jetbrains.com/pycharm/download 
+Sublime Text: https://www.sublimetext.com/3 
+Visual Studio Code https://code.visualstudio.com/Download 
+
+Python Interpreter
+open a command shell and type ‘python3’
+print(‘Hello World’)
+
+Anaconda manages “environments” 
+change upper right pane to file explorer
+Launch Spyder
+type:
+print(‘hello world’)
+<save>
+<hit play button> 
+
+Compiled vs Interpreted languages
+java, .Net etc… = compiled
+Python = interpreted 
+
+Compiled
+only needs to be compiled once
+typically executes faster
+must be redistributed whenever an update is released
+
+Interpreted
+is much more flexible
+is inherently slower
+must be interpreted every time it runs
+
+both compiled and interpreted have their place, so situation dictates which is preferred
+
+Common Applications of Python
+django - web dev 
+pygame - game dev toolkit
+matplotlib - machine learning
+scripted ops task
+
+DEVELOPMENT COMMUNITY
+PyPi = Python Package Index
+site full of reusable python modules for open use
+
+PIE AND PYTHON BASICS
+
+COMMENTS
+use comments! start everything with a comment
+# this is a comment
+print(‘this is a line of code’) 
+comments can be used to help you debug an error
+
+sometimes the best way to solve a problem is to break down a problem into smaller incremental steps
+comments can be used for that like: 
+
+# eat cookies 
+
+# 1. go to the store
+
+# 2. buy cookies
+
+# 3. eat cookies while watching Star Wars 
+
+VARIABLES 
+
+used to store a value
+names can start with letters or underscores and can contain numbers 
+should NOT be a Python KEYWORD 
+basic types are numbers, strings, and booleans 
+
+# USE COMMUNITY PACKAGES
+pip = pip installs package
+# go to “Environments” tab in Anaconda
+hit drop down for “not installed” 
+check a box and click apply and it will pull it down for you
+
+once a package is installed you can use it in your editor
+import <package> [as <alias>]
+
+the biggest difference between Python2 and Python3 is Python3 is fully Object Oriented
+Python2 is still just a plain scripting language
+
+# JSON and PYTHON go well together 
+
+
+
+
